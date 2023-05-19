3e45575b93e80ebbc9ac54f1690e130a9c69658e
lilocruz <superlinux.michael5@gmail.com>
2023-05-19 16:11:21 -0400

    List bucket function added, and create bucket function was improved

--- sarmastack.py
+++ sarmastack.py
@@ -6,6 +6,7 @@
 
 import argparse
 import boto3
+import botocore.exceptions
 import yaml
 
 def create_instance(args):
@@ -69,13 +70,68 @@ def delete_instance(args):
 
 def create_bucket(args):
     s3_client = boto3.client('s3')
-    response = s3_client.create_bucket(
-        Bucket=args['name'],
-        CreateBucketConfiguration={
-            'LocationConstraint': args['region']
-        }
-    )
-    print(f"Created bucket: args{['bucket_name']}")
+
+    bucket_name = args.get('bucket_name')
+    region = args.get('region')
+
+    if bucket_name and region:
+        location_constraint = get_location_constraint(region)
+
+        try:
+            if location_constraint:
+                response = s3_client.create_bucket(
+                    Bucket=bucket_name,
+                    CreateBucketConfiguration={
+                        'LocationConstraint': location_constraint
+                    }
+                )
+            else:
+                response = s3_client.create_bucket(Bucket=bucket_name)
+            
+            print(f"Created bucket {bucket_name} in region {region}")
+        except botocore.exceptions.ClientError as e:
+            error_code = e.response['Error']['Code']
+            error_message = e.response['Error']['Message']
+            if error_code == 'BucketAlreadyExists':
+                print(f"Bucket {bucket_name} already exists.")
+            else:
+                print(f"Error occurred while creating the bucket: {error_message}")
+        except Exception as e:
+            print(f"Error occurred while creating the bucket: {str(e)}")
+    else:
+        print("Please provide both 'bucket_name' and 'region' arguments.")
+    
+
+def list_buckets(args):
+    s3_client = boto3.client('s3')
+    
+    try:
+        response = s3_client.list_buckets()
+        buckets = response['Buckets']
+        
+        if buckets:
+            print("List of buckets:")
+            for bucket in buckets:
+                bucket_name = bucket['Name']
+                creation_date = bucket['CreationDate']
+                print(f"- {bucket_name} (Created on: {creation_date})")
+        else:
+            print("No buckets found.")
+    except Exception as e:
+        print(f"Error occurred while listing buckets: {str(e)}")
+
+def get_location_constraint(region):
+    # AWS region to location constraint mapping
+    region_mapping = {
+        'us-east-1': '',  # Empty string for us-east-1 (N. Virginia)
+        'us-east-2': 'us-east-2',
+        'us-west-1': 'us-west-1',
+        'us-west-2': 'us-west-2',
+        'eu-west-1': 'EU',
+        # Add more region mappings as needed
+    }
+
+    return region_mapping.get(region, region)
 
 def delete_bucket(args):
     s3_client = boto3.client('s3')
@@ -183,6 +239,11 @@ def main():
     # Create a parser for the "create-bucket" command
     create_bucket_parser = subparsers.add_parser('create-bucket', help='Create a bucket')
     create_bucket_parser.add_argument('-bn', '--bucket_name', help='Name of the bucket')
+    create_bucket_parser.add_argument('-rg', '--region', help='AWS region')
+    create_bucket_parser.add_argument('-f', '--file', help='Path to the YAML file')
+
+    # Create a parser for the "list-bucket" commands
+    list_bucke_parser = subparsers.add_parser('list-bucket', help='List bucket')
 
     # Create a parser for the "delete-bucket" command
     delete_bucket_parser = subparsers.add_parser('delete-bucket', help='Delete a bucket')
@@ -209,6 +270,8 @@ def main():
         delete_instance(args)
     elif args['command'] == 'create-bucket':
         create_bucket(args)
+    elif args['command'] == 'list-bucket':
+        list_buckets(list_bucke_parser)
     elif args['command'] == 'delete-bucket':
         delete_bucket(args)
     elif args['command'] == 'create-iam-user':
