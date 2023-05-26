3984d2a6839cd968498e605424638dce83eb42e6
lilocruz <superlinux.michael5@gmail.com>
2023-05-26 01:47:46 -0400

    Working on the implementation of the PLAN feature

--- sarmastack.py
+++ sarmastack.py
@@ -50,8 +50,11 @@ def provision(args):
         for instance in instances:
             instance_name = instance.get('instance_name')
             if not state_tracker.resource_exists('instances', instance_name):
-                create_manager.create_instance(instance)
-                state_tracker.update_resource_state('instances', instance_name, {})
+                if args.get('build'):
+                    print(f"Would create instance: {instance_name}")
+                else:
+                    create_manager.create_instance(instance)
+                    state_tracker.update_resource_state('instances', instance_name, {})
             else:
                 print(f"Instance '{instance_name}' already exists. Skipping creation.")
 
@@ -60,8 +63,11 @@ def provision(args):
         for bucket in buckets:
             bucket_name = bucket.get('bucket_name')
             if not state_tracker.resource_exists('buckets', bucket_name):
-                create_manager.create_bucket(bucket)
-                state_tracker.update_resource_state('buckets', bucket_name, {})
+                if args.get('build'):
+                    print(f"Would create bucket: {bucket_name}")
+                else:
+                    create_manager.create_bucket(bucket)
+                    state_tracker.update_resource_state('buckets', bucket_name, {})
             else:
                 print(f"Bucket '{bucket_name}' already exists. Skipping creation.")
 
@@ -72,28 +78,38 @@ def provision(args):
             resource_name = resource.get('name')
             if resource_type == 'iam_user':
                 if not state_tracker.resource_exists('iam_users', resource_name):
-                    create_manager.create_iam_user(resource)
-                    state_tracker.update_resource_state('iam_users', resource_name, {})
+                    if args.get('build'):
+                        print(f"Would create IAM user: {resource_name}")
+                    else:
+                        create_manager.create_iam_user(resource)
+                        state_tracker.update_resource_state('iam_users', resource_name, {})
                 else:
                     print(f"IAM user '{resource_name}' already exists. Skipping creation.")
             elif resource_type == 'iam_role':
                 if not state_tracker.resource_exists('iam_roles', resource_name):
-                    role_name = resource.get('role_name')
-                    assume_role_policy = resource.get('assume_role_policy')
-                    create_manager.create_iam_role(role_name, assume_role_policy)
-                    state_tracker.update_resource_state('iam_roles', resource_name, {})
+                    if args.get('build'):
+                        print(f"Would create IAM role: {resource_name}")
+                    else:
+                        role_name = resource.get('role_name')
+                        assume_role_policy = resource.get('assume_role_policy')
+                        create_manager.create_iam_role(role_name, assume_role_policy)
+                        state_tracker.update_resource_state('iam_roles', resource_name, {})
                 else:
                     print(f"IAM role '{resource_name}' already exists. Skipping creation.")
             elif resource_type == 'iam_policy':
                 if not state_tracker.resource_exists('iam_policies', resource_name):
-                    create_manager.create_iam_policy(resource)
-                    state_tracker.update_resource_state('iam_policies', resource_name, {})
+                    if args.get('build'):
+                        print(f"Would create IAM policy: {resource_name}")
+                    else:
+                        create_manager.create_iam_policy(resource)
+                        state_tracker.update_resource_state('iam_policies', resource_name, {})
                 else:
                     print(f"IAM policy '{resource_name}' already exists. Skipping creation.")
             else:
                 print(f"Unsupported resource type: {resource_type}")
 
     state_tracker.save_state()
+
 
 def main():
     
@@ -228,7 +244,7 @@ def main():
     # Create a parser for the "provision" command
     provision_parser = subparsers.add_parser('provision', help='Provision infrastructure from YAML file')
     provision_parser.add_argument('-f', '--file', help='Path to the YAML file')
-
+    
     # Parse the command-line arguments
     args = vars(parser.parse_args())
 
